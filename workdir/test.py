152236bb598ee7e7230920d6f45bccfb784a9441
koskot77 <koskot77@server.fake>
2014-12-01 06:00:56 -0500

    simplification

--- workdir/test.py
+++ workdir/test.py
@@ -1,46 +1,15 @@
-from ctypes import cdll
-lib = cdll.LoadLibrary('./AppFramework.so')
+from init import *
 
-class AppFramework(object):
-    def __init__(self):
-        self.obj = lib.AppFramework_new()
-
-    def __init__(self,seq):
-        self.obj = lib.AppFramework_new(seq)
-
-    def modList(self):
-        lib.AppFramework_modList(self.obj)
-
-    def verbose(self,target,action):
-        lib.AppFramework_verbose(self.obj,target,action)
-
-    def getParameter(self,modparname):
-        lib.AppFramework_getParameter(self.obj,modparname)
-
-    def modify(self,modparname,parvalue):
-        lib.AppFramework_modify(self.obj,modparname,parvalue)
-
-    def process(self,number):
-        lib.AppFramework_process(self.obj,number)
-
-    def beginJob(self):
-        lib.AppFramework_beginJob(self.obj)
-
-    def endJob(self):
-        lib.AppFramework_endJob(self.obj)
-
-if __name__ == '__main__':
-
-    fw = AppFramework('InputModule->BasicReader->GeneratorReader->MuonReader->ElectronReader->JetReader->METReader->Analyser->OutputModule')
-    fw.verbose("AppFramework","cout on") 
-    fw.modList();
-    fw.modify("InputModule::path","/tmp/kkotov/MonotopToHad_S3_MSM-600_Tune4C_13TeV-madgraph-tauola.root");
-    fw.modify("InputModule::showProgressPeriod","100");
-    fw.modify("InputModule::firstEntry","0");
-    fw.modify("OutputModule::output","qwe.csv");
-    fw.modify("OutputModule::leaflist","numberOfJets/I:m3jets/D:mmm/D");
-    fw.modify("Analyser::output","qwe.root");
-    fw.beginJob();
-    fw.process(10);
-    fw.endJob();
-
+#fw = AppFramework('InputModule->BasicReader->GeneratorReader->MuonReader->ElectronReader->JetReader->METReader->Analyser->OutputModule')
+fw = AppFramework('InputModule->BasicReader->GeneratorReader->JetReader->Analyser->OutputModule')
+fw.verbose("AppFramework","cout on") 
+fw.modList();
+fw.modify("InputModule::path","/store/user/kkotov/BN_MonotopS3_MSM-500_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_0/MonotopS3_MSM-500_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM_1_3_19w.root");
+fw.modify("InputModule::showProgressPeriod","100");
+fw.modify("InputModule::firstEntry","0");
+fw.modify("OutputModule::output","qwe.csv");
+fw.modify("OutputModule::leaflist","numberOfJets/I:m3jets/D:mmm/D");
+fw.modify("Analyser::output","qwe.root");
+fw.beginJob();
+fw.process(10);
+fw.endJob();
