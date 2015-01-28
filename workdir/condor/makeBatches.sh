973b06cd5906ff88d36d21ee02b48c6192d70958
koskot77 <koskot77@server.fake>
2015-01-28 04:58:30 -0500

    qwe

--- workdir/condor/makeBatches.sh
+++ workdir/condor/makeBatches.sh
@@ -8,7 +8,7 @@ for p in $processes; do
    while [ $i -lt $splitLevel ] ; do
       s=$i"of"$splitLevel
       i=`expr $i + 1`
-      arguments="-s$s -i$p -o/data/users/kkotov/2015.01.27/"
+      arguments="-s$s -i$p -o/tmp/"
       cat batch.jdf | sed -e "s|ARGS|$arguments|g" -e "s|OUT|$p$s|g" -e "s|ERR|$p$s|g" -e "s|LOG|$p$s|g" > tmp/batch$p""$s.jdf
    done;
 done;
