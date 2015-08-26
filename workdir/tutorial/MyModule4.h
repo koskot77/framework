8f6fbaaddeb6633a7c20844b0ad3e885264f8572
koskot77 <koskot77@hotmail.com>
2015-08-26 23:55:01 +0200

    qwe

--- workdir/tutorial/MyModule4.h
+++ workdir/tutorial/MyModule4.h
@@ -12,21 +12,12 @@ private:
         AppParameter<double> mydouble;
         AppParameter<string> mystring;
 
-        AppResult beginJob(AppEvent& event){ cout<<"job begin"<<endl; return AppResult(); }
+        AppResult beginJob(AppEvent& event){ cout<<"job begin: mydouble="<<mydouble<<" mystring="<<mystring<<endl; return AppResult(); }
         AppResult beginRun(AppEvent& event){ cout<<"run begin"<<endl; return AppResult(); }
         AppResult endRun  (AppEvent& event){ cout<<"run ended"<<endl; return AppResult(); }
         AppResult endJob  (AppEvent& event){ cout<<"job ended"<<endl; return AppResult(); }
 
-        AppResult event (AppEvent& event){
-//                std::cout<<"Event "<<myint<<endl;
-//                switch( myint++ ){
-//                       case 3 : mydouble = 321.123; return AppResult();
-//                       case 5 : return AppResult(AppResult::SKIP|AppResult::WARNING,"skipped");
-//                       case 7 : return AppResult(AppResult::STOP|AppResult::ERROR,"stopped");
-//                       default : cout<<"mydouble: "<<mydouble<<endl; break;
-//                }
-                return AppResult();
-        }
+        AppResult event (AppEvent& event){ return AppResult(); }
 
 public:
         string getMyString(void){ return mystring; }
